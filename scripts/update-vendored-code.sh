b6564747984129470ab85a77cba6e7ad41c4f93b
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-03-16 17:14:40 -0500

    Update vendor script

--- scripts/update-vendored-code.sh
+++ scripts/update-vendored-code.sh
@@ -8,5 +8,8 @@ targetDir="$root/src/CliWrap.Tests.Dummy"
 
 git submodule foreach --recursive git clean -xfd
 rm -rf "$targetDir" || echo 'Target is clean'
+
+[ '--clean' == "${1:-}" ] && exit 0
+
 cp -r "$vendorDir/CliWrap.Tests.Dummy" "$targetDir"
 patch "$targetDir/CliWrap.Tests.Dummy.csproj" <"$patch"
