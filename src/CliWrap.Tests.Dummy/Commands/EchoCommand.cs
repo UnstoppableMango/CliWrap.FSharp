ce83811c4e715fff15ea1cc5ca0230199e0ca58c
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-03-16 17:17:32 -0500

    vendor the dummy project a different way

---
+++ src/CliWrap.Tests.Dummy/Commands/EchoCommand.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CliFx;
+using CliFx.Attributes;
+using CliFx.Infrastructure;
+using CliWrap.Tests.Dummy.Commands.Shared;
+
+namespace CliWrap.Tests.Dummy.Commands;
+
+[Command("echo")]
+public class EchoCommand : ICommand
+{
+    [CommandParameter(0)]
+    public required IReadOnlyList<string> Items { get; init; }
+
+    [CommandOption("target")]
+    public OutputTarget Target { get; init; } = OutputTarget.StdOut;
+
+    [CommandOption("separator")]
+    public string Separator { get; init; } = " ";
+
+    public async ValueTask ExecuteAsync(IConsole console)
+    {
+        foreach (var writer in console.GetWriters(Target))
+            await writer.WriteLineAsync(string.Join(Separator, Items));
+    }
+}
---
+++ src/CliWrap.Tests.Dummy/Commands/SleepCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using CliFx;
+using CliFx.Attributes;
+using CliFx.Infrastructure;
+
+namespace CliWrap.Tests.Dummy.Commands;
+
+[Command("sleep")]
+public class SleepCommand : ICommand
+{
+    [CommandParameter(0)]
+    public TimeSpan Duration { get; init; } = TimeSpan.FromSeconds(1);
+
+    public async ValueTask ExecuteAsync(IConsole console)
+    {
+        var cancellationToken = console.RegisterCancellationHandler();
+
+        try
+        {
+            await console.Output.WriteLineAsync($"Sleeping for {Duration}...");
+            await Task.Delay(Duration, cancellationToken);
+        }
+        catch (OperationCanceledException)
+        {
+            await console.Output.WriteLineAsync("Canceled.");
+            return;
+        }
+
+        await console.Output.WriteLineAsync("Done.");
+    }
+}
