aea82edb7ed38ad1d1dc9cd9f8f5762ebaed0481
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-03-16 17:11:22 -0500

    Use patch instead of git

--- scripts/update-vendored-code.sh
+++ scripts/update-vendored-code.sh
@@ -2,4 +2,11 @@
 set -eum
 
 root="$(git rev-parse --show-toplevel)"
-ln -s "$root/vendor/CliWrap/CliWrap.Tests.Dummy" "$root/src"
+patch="$root/patches/0001-CliWrap.Tests.Dummy.csproj.patch"
+vendorDir="$root/vendor/CliWrap"
+targetDir="$root/src/CliWrap.Tests.Dummy"
+
+git submodule foreach --recursive git clean -xfd
+rm -rf "$targetDir" || echo 'Target is clean'
+cp -r "$vendorDir/CliWrap.Tests.Dummy" "$targetDir"
+patch "$targetDir/CliWrap.Tests.Dummy.csproj" <"$patch"
