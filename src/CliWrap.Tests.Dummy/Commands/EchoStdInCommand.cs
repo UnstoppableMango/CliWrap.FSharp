5e382e900e754e480a7eaed12c3a66779e2d718e
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-02 15:42:12 -0500

    Generate new dummy project

---
+++ src/CliWrap.Tests.Dummy/Commands/EchoStdInCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Buffers;
+using System.Threading.Tasks;
+using CliFx;
+using CliFx.Attributes;
+using CliFx.Infrastructure;
+using CliWrap.Tests.Dummy.Commands.Shared;
+
+namespace CliWrap.Tests.Dummy.Commands;
+
+[Command("echo stdin")]
+public class EchoStdInCommand : ICommand
+{
+    [CommandOption("target")]
+    public OutputTarget Target { get; init; } = OutputTarget.StdOut;
+
+    [CommandOption("length")]
+    public long Length { get; init; } = long.MaxValue;
+
+    public async ValueTask ExecuteAsync(IConsole console)
+    {
+        using var buffer = MemoryPool<byte>.Shared.Rent(81920);
+
+        var totalBytesRead = 0L;
+        while (totalBytesRead < Length)
+        {
+            var bytesWanted = (int)Math.Min(buffer.Memory.Length, Length - totalBytesRead);
+
+            var bytesRead = await console.Input.BaseStream.ReadAsync(buffer.Memory[..bytesWanted]);
+            if (bytesRead <= 0)
+                break;
+
+            foreach (var writer in console.GetWriters(Target))
+                await writer.BaseStream.WriteAsync(buffer.Memory[..bytesRead]);
+
+            totalBytesRead += bytesRead;
+        }
+    }
+}
---
+++ src/CliWrap.Tests.Dummy/Commands/GenerateBinaryCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Buffers;
+using System.Threading.Tasks;
+using CliFx;
+using CliFx.Attributes;
+using CliFx.Infrastructure;
+using CliWrap.Tests.Dummy.Commands.Shared;
+
+namespace CliWrap.Tests.Dummy.Commands;
+
+[Command("generate binary")]
+public class GenerateBinaryCommand : ICommand
+{
+    // Tests rely on the random seed being fixed
+    private readonly Random _random = new(1234567);
+
+    [CommandOption("target")]
+    public OutputTarget Target { get; init; } = OutputTarget.StdOut;
+
+    [CommandOption("length")]
+    public long Length { get; init; } = 100_000;
+
+    [CommandOption("buffer")]
+    public int BufferSize { get; init; } = 1024;
+
+    public async ValueTask ExecuteAsync(IConsole console)
+    {
+        using var buffer = MemoryPool<byte>.Shared.Rent(BufferSize);
+
+        var totalBytesGenerated = 0L;
+        while (totalBytesGenerated < Length)
+        {
+            _random.NextBytes(buffer.Memory.Span);
+
+            var bytesWanted = (int)Math.Min(buffer.Memory.Length, Length - totalBytesGenerated);
+
+            foreach (var writer in console.GetWriters(Target))
+                await writer.BaseStream.WriteAsync(buffer.Memory[..bytesWanted]);
+
+            totalBytesGenerated += bytesWanted;
+        }
+    }
+}
