5a8efa73df04df9e1c59f19d555792fc5f00268a
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-02-10 23:01:41 -0600

    WIP

--- flake.nix
+++ flake.nix
@@ -1,15 +1,20 @@
 {
-  description = "A very basic flake";
+  description = "";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
+      };
+    };
   };
 
   outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
   };
 }
